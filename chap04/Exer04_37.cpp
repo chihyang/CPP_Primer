9d5bab77d660f9420f57916cdf04773b652833d8
chihyang <chihyanghsin@gmail.com>
2015-10-07 17:07:45 +0800

    Correct comments in exercise 4.37.

--- chap04/Exer04_37.cpp
+++ chap04/Exer04_37.cpp
@@ -13,13 +13,13 @@ int main()
 	const string *ps = &s;
 	char *pc = &c;
 	void *pv;
-	pv = const_cast<string*>(ps);
 	// As long as we cast away const, we can assign a pointer to void*.
+	pv = const_cast<string*>(ps);
+	// Implicit conversion gets the same result as below.
 	i = static_cast<int>(*pc);
-	// Implicit conversion is the same.
-	pv = &d;
 	// Assigning a pointer to void* directly is okay.
+	pv = &d;
+	// Getting original pointer type can be performed by static_cast.
 	pc = static_cast<char*>(pv);
-	// Getting original pointer type could be performed by static_cast.
 	return 0;
 }
