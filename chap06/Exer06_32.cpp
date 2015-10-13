de01ec4018845bb56f9098cf253b726523f3a095
chihyang <chihyanghsin@gmail.com>
2015-10-13 18:04:45 +0800

    Exercise 6.30 to 6.33.

---
+++ chap06/Exer06_32.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+int &get(int *arry, int index)
+{
+	return arry[index];
+}
+int main()
+{
+	int ia[10];
+	for(int i = 0; i != 10; ++i)
+		get(ia, i) = i;
+	for(const auto ele : ia)
+		cout << ele << " ";
+	cout << endl;
+}
---
+++ chap06/Exer06_33.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using std::cout;
+using std::endl;
+using std::vector;
+void printVec(const vector<int> &vec, int beg, int size)
+{
+	if(vec.empty())
+		return;
+	if(size - beg > 0)
+	{
+		cout << vec[beg] << " ";
+		printVec(vec, ++beg, size);
+	}
+}
+int main()
+{
+	vector<int> vi = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	printVec(vi, 0, vi.size());
+	cout << endl;
+	return 0;
+}
