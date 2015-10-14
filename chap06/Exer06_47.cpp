139f76fa1a58d88d4fcf1e3f02f4481fc2405331
chihyang <chihyanghsin@gmail.com>
2015-10-14 22:24:30 +0800

    Exercise 6.47.

---
+++ chap06/Exer06_47.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <cassert>
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::vector;
+void printVec(const vector<int> &vec, int beg, int size)
+{
+	if(vec.empty())
+		return;
+	if(size - beg > 0)
+	{
+		#ifndef NDEBUG
+		cerr << "\n" << __FUNCTION__ << ":\n"
+		     << "The size of the vector:" << size - beg << endl;
+		#endif
+		cout << vec[beg] << " ";
+		printVec(vec, ++beg, size);
+	}
+}
+int main()
+{
+	vector<int> vi = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	printVec(vi, 0, vi.size());
+	cout << endl;
+	return 0;
+}
+// Note: for visual studio compiler, substitute __FUNCTION__ for __func__,
+// for g++, both __FUNCTION__ and __func__ are okay.
