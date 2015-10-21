6c5dba6d6bb91ca5b06b917b23369c1dec646d6d
chihyang <chihyanghsin@gmail.com>
2015-10-21 18:18:18 +0800

    Exercise 7.57 and 7.58.

---
+++ chap07/example.cpp
@@ -0,0 +1,7 @@
+#include "example.h"
+double Example::rate = 6.5;
+vector<double> Example::vec(vecSize);
+int main()
+{
+	return 0;
+}
---
+++ chap07/example.h
@@ -0,0 +1,8 @@
+#include <vector>
+using std::vector;
+class Example {
+public:
+	static double rate;
+	static const int vecSize = 20;
+	static vector<double> vec;
+};
