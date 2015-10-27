0724fdb3dcef7e1f22e9d7a34572da8a28b85fb9
chihyang <chihyanghsin@gmail.com>
2015-10-27 22:10:47 +0800

    Ways to construct a string in exercise 9.41.

--- chap09/Exer09_41.cpp
+++ chap09/Exer09_41.cpp
@@ -9,8 +9,11 @@ int main()
 {
 	vector<char> vch = { 'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!', '!', '!' };
 	char* cp = &vch[0];
-	string str(cp, vch.size()); // don't know why, even if we don't provide vch.size(), the result still 
+	// way1, don't forget sting can be initialized with common way of a container
+	string str1(vch.cbegin(), vch.cend());
+	string str2(cp, vch.size()); // don't know why, even if we don't provide vch.size(), the result still 
 	// the same
-	cout << str << endl;
+	cout << str1 << endl;
+	cout << str2 << endl;
 	return 0;
 }
