1ed7b1a4e721644e788130325ac75c53e6f28933
chihyang <chihyanghsin@gmail.com>
2015-10-28 13:26:41 +0800

    Exercise 9.43 to 9.46.

---
+++ chap09/Exer09_43.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using std::cout;
+using std::endl;
+using std::string;
+string& changeSubstr(string &s, const string& oldVal, const string& newVal)
+{
+	if(s.empty() || oldVal.empty()) // be careful, s and oldVal should be non empty strings
+		return s;
+	auto sizeOld = oldVal.size();
+	auto sizeNew = newVal.size();
+	auto iter = s.begin();
+	while(iter != s.end()) // make sure don't skip any element
+	{
+		string subOld(iter, iter + sizeOld);
+		if(subOld == oldVal)
+		{
+			s.erase(iter, iter + sizeOld);
+			iter = s.insert(iter, newVal.begin(), newVal.end());
+			iter += sizeNew;
+		}
+		else
+			++iter;
+	}
+	return s;
+}
+int main()
+{
+	string s = "I came thru the way. thru thru thru";
+	cout << s << endl;
+	changeSubstr(s, "", "");
+	cout << s << endl;
+	return 0;
+}
---
+++ chap09/Exer09_45.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using std::cout;
+using std::endl;
+using std::string;
+string& address(string& name, const string& prefix, const string& suffix)
+{
+	if(name.empty() && prefix.empty() && suffix.empty())
+		return name;
+	name.insert(name.begin(), prefix.begin(), prefix.end());
+	name.append(suffix);
+	return name;
+}
+int main()
+{
+	string lady = "Christie";
+	string gentleman;
+	address(gentleman, "", "");
+	cout << lady << endl;
+	cout << gentleman << endl;
+	return 0;
+}
