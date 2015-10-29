33578ebf00649ae170b799160a72c4f93e6a3a19
chihyang <chihyanghsin@gmail.com>
2015-10-29 17:19:22 +0800

    Store the processing result in exercise 9.52.

--- chap09/Exer09_52.cpp
+++ chap09/Exer09_52.cpp
@@ -8,8 +8,9 @@ using std::string;
 int main()
 {
 	string s("3+2*(8/4)+(15/(3+(4/(234+23)+8)/2))");
+	string after; // store the result
 	stack<string> expresion;
-	string after_proess;
+	string in_bracket;
 	bool open;
 	for(string::size_type i = 0; i != s.size(); ++i)
 	{
@@ -21,6 +22,7 @@ int main()
 				expresion.push(ch); // nested parenthesis is pushed into stack as a mark
 			}
 			open = true;
+			after += ch;
 			continue;
 		}
 		if(ch == ")")
@@ -32,15 +34,19 @@ int main()
 					expresion.pop();
 					break;
 				}
-				after_proess += expresion.top();
+				in_bracket += expresion.top();
 				expresion.pop();
 			}
 			open = !expresion.empty();
+			after = after + in_bracket + ch;
+			in_bracket.clear();
 			continue;
 		}
 		if(open)
 			expresion.push(ch);
+		else
+			after += ch;
 	}
-	cout << after_proess << endl;
+	cout << after << endl;
 	return 0;
 }
