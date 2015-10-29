54207a59c8edd43a4cd7fe90c39da3f4176035cc
chihyang <chihyanghsin@gmail.com>
2015-10-29 10:18:19 +0800

    Enhance constructor of date class in Exer09_51.h.

--- chap09/Exer09_51.cpp
+++ chap09/Exer09_51.cpp
@@ -1,15 +1,31 @@
 #include <iostream>
 #include <string>
+#include <stdexcept>
 #include "Exer09_51.h"
 using std::cin;
+using std::cerr;
+using std::endl;
 using std::string;
+using std::invalid_argument;
 int main()
 {
 	string d;
 	while(getline(cin, d))
 	{
-		Date date(d);
-		date.print_date();
+		try
+		{
+			Date date(d);
+			date.print_date();
+		}
+		catch(invalid_argument inv_arg)
+		{
+			cerr << inv_arg.what()
+			     << "\nTry again? Enter y or n" << endl;
+			char c;
+			cin >> c;
+			if(!cin || c == 'n')
+				break;
+		}
 	}
 	return 0;
 }
--- chap09/Exer09_51.h
+++ chap09/Exer09_51.h
@@ -1,4 +1,3 @@
-// Note: this is a quite complicated implementation, it's a temporary solution.
 #ifndef EXER09_51_H
 #define EXER09_51_H
 #include <iostream>
@@ -10,7 +9,6 @@ using std::endl;
 using std::string;
 using std::vector;
 using std::invalid_argument;
-using std::out_of_range;
 vector<string> MONALL{ "JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE",
                        "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"};
 class Date{
@@ -27,19 +25,19 @@ private:
 // Date format could be surprisingly complicated. See the wiki page for more info:
 // https://en.wikipedia.org/wiki/Date_format_by_country
 // Here, the constructor can only take one argument. So I suppose the order is
-// mdy.
-Date::Date(const string& date)
+// month-date-year.
+Date::Date(const string& date) : Date()
 {
 	unsigned year_temp = 0, month_temp = 0, day_temp = 0;
-	if(date.empty())
+	if(date.empty()) // empty string is invalid
 		throw invalid_argument("Argument shouldn't be empty");
-	vector<string> split; // store non-white-spaces and non-separators
+	vector<string> split; // put valid strings into a vector
 	string str;
 	// filter white spaces and separators
 	for(const auto &c : date)
 	{
 		if(!(isspace(c) || c == ',' || c == '-' || c == '/' || c == '.'))
-			str.push_back(c);
+			str.push_back(c); // any separators above (including contiguous white spaces) are okay
 		else
 		{
 			if(!str.empty())
@@ -49,54 +47,64 @@ Date::Date(const string& date)
 			}
 		}
 	}
-	split.push_back(str);
+	split.push_back(str); // don't forget to push back the last result
 	if(split.size() != 3)
 		throw invalid_argument("Not a valid date.");
 	// if month begins with a letter, it might be a month in English
 	if(isalpha(split[0][0]))
 	{
 		for(auto &c : split[0])
-			c = toupper(c);
+			c = toupper(c); // convert month to upper case
 		for(decltype(MONALL.size()) m = 0; m <= MONALL.size(); ++m)
 		{
 			if(MONALL[m].find(split[0]) == 0) // abbreviation or full
 			{
-				month_temp = m + 1;
-				break;
-			}
+				if(split[0].size() >= 3 && split[0].size() <= MONALL[m].size())
+				{
+					month_temp = m + 1;
+					break;
+				}
+				else
+					throw invalid_argument("Not a valid month.");
+			}				
+			if(m == 11)
+				throw invalid_argument("Not a valid month.");
 		}
 	}
 	// or month should be a numeric value
 	else if(isdigit(split[0][0]))
+	{
 		month_temp = stoi(split[0]);
+		if(month_temp < 1 || month_temp > 12)
+			throw invalid_argument("Not a valid month.");
+	}
 	// or the input month is wrong
 	else
-		throw invalid_argument("Month is invalid.");
+		throw invalid_argument("Input is not a month.");
 	day_temp = stoi(split[1]);
 	year_temp = stoi(split[2]);
-	// Judge is a date is legal
-	if(month_temp != 2 && (( month_temp <= 7 && month_temp % 2) || !(month_temp % 2)))
-	{
-		if(day_temp > 31)
-			throw invalid_argument("Date is invalid.");
-	}
-	else if(month_temp == 2)
-	{
-		if(isleap(year_temp))
-		{	
-			if(day_temp > 29)
-				throw invalid_argument("Date is invalid for leap year.");
-		}
-		else
-		{
-			if(day_temp > 28)
-				throw invalid_argument("Date is invalid for non-leap year.");
-		}
-	}
-	else
-	{
-		if(day_temp > 31)
-			throw out_of_range("Date is out of range.");
+	// Judge if a date is legal
+	switch(month_temp){
+		case 2:	
+			if(isleap(year_temp))
+			{	
+				if(day_temp > 29)
+					throw invalid_argument("Date is invalid for leap year.");
+			}
+			else
+			{
+				if(day_temp > 28)
+					throw invalid_argument("Date is invalid for non-leap year.");
+			}
+			break;
+		case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+			if(day_temp > 31)
+				throw invalid_argument("Date is invalid.");
+			break;
+		case 4: case 6: case 9: case 11: default:
+			if(day_temp > 30)
+				throw invalid_argument("Date is out of range.");
+			break;
 	}
 	year = year_temp;
 	month = month_temp;
@@ -112,3 +120,18 @@ inline bool Date::isleap(unsigned y)
 	return ((y % 400 == 0) || (y % 100 != 0 && y % 4 == 0));
 }
 #endif
+// Note 1: basic thought is to split the argument into different substrings that
+// contains no white spaces and then process each substring and verify if a date
+// is legal.
+
+// Note 2: the constructor above tries its best to handle exceptions, thus leading
+// to throwing many exceptions at different places. This makes the constructor 
+// ugly. So is it appropriate to throw exceptions in a constructor? The answer
+// seems yes from 
+// https://stackoverflow.com/questions/77639/when-is-it-right-for-a-constructor-to-throw-an-exception
+// and
+// https://stackoverflow.com/questions/810839/throwing-exceptions-from-constructors
+// After all, to get a valid result, we have to judge whether an input that 
+// represents a date is valid. If these exception handlers were not in a 
+// constructor, we have to put them where a date input is required. That means
+// we have to either write another function or do repeated work.
