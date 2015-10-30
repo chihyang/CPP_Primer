574cd6afebaa8860e56e441f08e617a0670685ae
chihyang <chihyanghsin@gmail.com>
2015-10-30 16:49:31 +0800

    Verification of exercise 10.4.

---
+++ chap10/Exer10_04.cpp
@@ -0,0 +1,14 @@
+// Warning: this is for verification. Don't use accumulate this way.
+#include <iostream>
+#include <vector>
+#include <numeric>
+using std::cout;
+using std::endl;
+using std::vector;
+int main()
+{
+	vector<double> vi = { 7, 19.8, 3.9, .429, 34.5, 4.40, 6.6, 43.2, 1.7, 5.00, 3.31 };
+	auto sum = accumulate(vi.cbegin(), vi.cend(), 0);
+	cout << sum << endl;
+	return 0;
+}
