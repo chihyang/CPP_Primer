c195c127e1f178aac365f4d01115038095ea9f80
chihyang <chihyanghsin@gmail.com>
2015-10-31 19:49:27 +0800

    Exercise 10.16 to 10.19.

---
+++ chap10/Exer10_17.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include "Sales_Data.h"
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+int main()
+{
+	vector<Sales_data> sales_item;
+	Sales_data item;
+	while(read(cin, item))
+	{
+		sales_item.push_back(item);
+	}
+	sort(sales_item.begin(), sales_item.end(),
+       	[](const Sales_data& sd1, const Sales_data& sd2){return sd1.isbn() < sd2.isbn();});
+	for(const auto &s : sales_item)
+	{
+		print(cout, s) << endl;
+	}
+	return 0;
+}
