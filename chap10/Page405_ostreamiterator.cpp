53dd4054cb0ec978614fc74fac6af58062d2473d
chihyang <chihyanghsin@gmail.com>
2015-11-02 11:41:22 +0800

    Add comments in ostream_iterator verification on page 405.

--- chap10/Page405_ostreamiterator.cpp
+++ chap10/Page405_ostreamiterator.cpp
@@ -11,7 +11,7 @@ int main()
 	vector<int> ivec{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 	ostream_iterator<int> out_iter(cout, " ");
 	for(auto e : ivec)
-		*out_iter++ = e;
+		*out_iter++ = e; // equivalent to out_iter = e, but more readable and general
 	cout << endl;
 	copy(ivec.begin(), ivec.end(), out_iter);
 	cout << endl;
