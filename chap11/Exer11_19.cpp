3ece8f6c2553b782237b12a4a69614ae3e60f768
chihyang <chihyanghsin@gmail.com>
2015-11-03 19:28:43 +0800

    Exercise 11.15 and 11.19.

---
+++ chap11/Exer11_19.cpp
@@ -0,0 +1,13 @@
+#include <set>
+#include "Sales_data.h"
+using std::multiset;
+bool compareIsbn(const Sales_data& lhs, const Sales_data& rhs)
+{
+	return lhs.isbn() < rhs.isbn();
+}
+int main()
+{
+	multiset<Sales_data, bool(*)(const Sales_data&, const Sales_data&)> bookstore(compareIsbn);
+	multiset<Sales_data, bool(*)(const Sales_data&, const Sales_data&)>::iterator bookstore_it = bookstore.begin();
+	return 0;
+}
