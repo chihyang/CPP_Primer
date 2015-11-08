37394b1509ef9030461d638e48f8e94dc139ad2e
chihyang <chihyanghsin@gmail.com>
2015-11-08 21:54:30 +0800

    Exercise 12.23 and 12.24.

---
+++ chap12/Exer12_24.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+int main()
+{
+	size_t sz = 8;
+	string str;
+	while(getline(cin, str))
+	{
+		char* ch_ptr = new char[str.size() + 1];
+		strcpy(ch_ptr, str.c_str());
+		cout << ch_ptr << endl;
+		delete[] ch_ptr;
+	}
+	return 0;
+}
