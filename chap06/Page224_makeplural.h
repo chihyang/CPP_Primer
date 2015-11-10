1255009c2a0f7f602a56ca8e275ea4eac9b38609
chihyang <chihyanghsin@gmail.com>
2015-11-10 18:23:37 +0800

    Define make_plural as a header in chapter 6.

---
+++ chap06/Page224_makeplural.h
@@ -0,0 +1,7 @@
+// This function will be used frequently by subsequent chapters.
+#include <string>
+using std::string;
+string make_plural(size_t ctr, const string &word, const string &ending)
+{
+	return (ctr > 1) ? word + ending : word;
+}
