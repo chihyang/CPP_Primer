e57717c8314b2a9a7a08c6f155d5ac6bd33a8221
chihyang <chihyanghsin@gmail.com>
2015-11-16 10:34:15 +0800

    Complement notes on the use of keyword default in Page264_default.cpp.

--- chap07/Page264_default.cpp
+++ chap07/Page264_default.cpp
@@ -19,3 +19,12 @@ int main()
 // default. If = default is added to a constructor that has default arguments(s),
 // the program won't compile. It's really confusing. This problem needs more 
 // consideration.
+
+// PS: Chapter 13 has a more detailed explanation on this problem:
+// We can use =default only on member functions that have a synthesized version.
+// See page 506 for more details. Simply speaking, when we use default, we are 
+// requiring the compiler to generate this function, thus of course we needn't 
+// write function body. Moreover, default needn't appear inside a class. It can 
+// also appear outside the class along with function definition--although called
+// "definition", this definition is special: it has no function body, just a 
+// function declaration followed by =default and a semicolon.
