5cd2a28d06735edab13a639e62c9b16a58154b08
chihyang <chihyanghsin@gmail.com>
2015-11-24 17:07:43 +0800

    Errata of comments in exercise 15.6 and 15.7.

--- chap15/Exer15_06.cpp
+++ chap15/Exer15_06.cpp
@@ -6,7 +6,7 @@ int main()
 {
 	Quote item1("978-7-121-20038-0", 128.0);
 	Bulk_quote item2("978-7-121-20038-0", 128.0, 6, 0.05);
-	print_total(cout, item1, 10); // calls Quote::neg_price
+	print_total(cout, item1, 10); // calls Quote::net_price
 	print_total(cout, item2, 10); // calls Bulk_quote::net_price
 	print_total(cout, item1, 5); // calls Quote::net_price
 	print_total(cout, item2, 5); // calls Bulk_quote::net_price
