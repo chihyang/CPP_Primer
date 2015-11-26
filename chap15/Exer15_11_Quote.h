30305412f5aa4b4f2a868314e196fddc233c9897
chihyang <chihyanghsin@gmail.com>
2015-11-26 19:03:50 +0800

    Comment correction in exercise 15.1, 15.11 and 15.15.

--- chap15/Exer15_11_Quote.h
+++ chap15/Exer15_11_Quote.h
@@ -25,7 +25,7 @@ double print_total(std::ostream &os, const Quote &item, std::size_t n)
 	// depending on the type of the object bound to the item parameter
 	// calls either Quote::net_price or Bulk_quote::net_price
 	double ret = item.net_price(n);
-	os << "ISBN: " << item.isbn() // calls QUote::isbn
+	os << "ISBN: " << item.isbn() // calls Quote::isbn
 	   << " # sold: " << n << " total due: " << ret << std::endl;
 	return ret;
 }
