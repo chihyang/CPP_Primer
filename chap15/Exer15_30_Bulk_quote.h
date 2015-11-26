af82f24b15a40fd349b7928e8f3de65f3208ca72
chihyang <chihyanghsin@gmail.com>
2015-11-26 21:06:19 +0800

    Exercise 13.30.
    Note: this exercise covers a useful way to dynamically allocate resources for objects of class in inheritance hierarchy.

---
+++ chap15/Exer15_30_Bulk_quote.h
@@ -0,0 +1,51 @@
+#ifndef BULK_QUOTE_H
+#define BULK_QUOTE_H
+#include <cstddef>
+#include <iostream>
+#include <string>
+#include "Exer15_30_Disc_quote.h"
+class Bulk_quote : public Disc_quote { // Bulk_quote inherits from Quote
+public:
+	// inherited constructor
+	using Disc_quote::Disc_quote;
+	Bulk_quote()=default;
+	// overrides the base version in order to implement the bulk purchase discount policy
+	double net_price(std::size_t) const override;
+	void debug() const override;
+	// copy-control members
+	Bulk_quote(const Bulk_quote &bq) : Disc_quote(bq) {}
+	Bulk_quote(Bulk_quote &&bq) noexcept : Disc_quote(std::move(bq)) {}
+	Bulk_quote& operator=(const Bulk_quote&);
+	Bulk_quote& operator=(Bulk_quote&&) noexcept;
+	~Bulk_quote()=default; // virtual by inheritance
+	virtual Bulk_quote* clone() const & { return new Bulk_quote(*this); }
+	virtual Bulk_quote* clone() && { return new Bulk_quote(std::move(*this)); }
+};
+// if the specified number of items are purchased, use the discounted price
+double Bulk_quote::net_price(std::size_t cnt) const
+{
+	if (cnt >= quantity)
+		return cnt * (1 - discount) * price;
+	else
+		return cnt * price;
+}
+inline void Bulk_quote::debug() const
+{
+	std::cout << "std::string bookNo\n"
+	          << "double price\n"
+	          << "std::size_t min_qty\n"
+	          << "double discount\n"
+	          << std::endl;
+}
+inline Bulk_quote& Bulk_quote::operator=(const Bulk_quote &rhs)
+{
+	Disc_quote::operator=(rhs);
+	return *this;
+}
+inline Bulk_quote& Bulk_quote::operator=(Bulk_quote &&rhs) noexcept
+{
+	if(this != &rhs)
+		Disc_quote::operator=(std::move(rhs));
+	return *this;
+}
+#endif
---
+++ chap15/Exer15_30_Quote.h
@@ -0,0 +1,64 @@
+#ifndef QUOTE_H
+#define QUOTE_H
+#include <cstddef>
+#include <iostream>
+#include <string>
+#include <utility>
+class Quote {
+public:
+	Quote()=default;
+	Quote(const std::string &book, double sales_price) : bookNo(book), price(sales_price) {}
+	std::string isbn() const { return bookNo; }
+	// return the total sales price for the specified number of items
+	// derived class will override and apply different discount algorithms
+	virtual double net_price(std::size_t n) const
+	    { return n * price; }
+	virtual void debug() const;
+	// copy control members
+	Quote(const Quote &q) : bookNo(q.bookNo), price(q.price) {}
+	Quote(Quote &&q) noexcept : bookNo(std::move(q.bookNo)), price(std::move(q.price)) {}
+	Quote& operator=(const Quote&);
+	Quote& operator=(Quote&&) noexcept;
+	virtual ~Quote()=default; // dynamic binding for the destructor
+	// virtual function to return a dynamically allocated copy of itself
+	// these members use reference qualifiers
+	virtual Quote* clone() const & { return new Quote(*this); }
+	virtual Quote* clone() && { return new Quote(std::move(*this)); }
+private:
+	std::string bookNo; // ISBN number of this item
+protected:
+	double price = 0.0; // normal, undiscounted price
+};
+// calculate and print the price for the given number of copies, applying any discount
+double print_total(std::ostream &os, const Quote &item, std::size_t n)
+{
+	// depending on the type of the object bound to the item parameter
+	// calls either Quote::net_price or Bulk_quote::net_price
+	double ret = item.net_price(n);
+	os << "ISBN: " << item.isbn() // calls Quote::isbn
+	   << " # sold: " << n << " total due: " << ret << std::endl;
+	return ret;
+}
+inline void Quote::debug() const
+{
+	std::cout << "std::string bookNo\n"
+	          << "double price\n"
+	          << std::endl;  
+}
+inline Quote& Quote::operator=(const Quote &rhs)
+{
+	// self-assignment is safe, we don't need to worry about it
+	bookNo = rhs.bookNo;
+	price = rhs.price;
+	return *this;
+}
+inline Quote& Quote::operator=(Quote &&rhs) noexcept
+{
+	if(this != &rhs)
+	{
+		bookNo = std::move(rhs.bookNo);
+		price = std::move(rhs.price);
+	}
+	return *this;
+}
+#endif
