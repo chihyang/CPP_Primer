ccc935468776c8977673ff12ce4228b4dfd3ccf3
chihyang <chihyanghsin@gmail.com>
2015-11-28 17:17:34 +0800

    Add more flexible tests in man file.

--- chap15/Exer15_39/Exer15_39.cpp
+++ chap15/Exer15_39/Exer15_39.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
+#include <string>
 #include "Exer15_39_TextQuery.h"
 #include "Exer15_39_Query.h"
 #include "Exer15_39_AndQuery.h"
 #include "Exer15_39_OrQuery.h"
 #include "Exer15_39_NotQuery.h"
 using std::cout;
+using std::cin;
 using std::endl;
 using std::ifstream;
+using std::string;
 int main(int argc, char* argv[])
 {
 	if(argc != 2)
 		return -1;
 	ifstream is(argv[1]);
 	TextQuery tq(is);
-	Query q = Query("fiery") & Query("bird") | Query("wind");
-	QueryResult qr = q.eval(tq);
-	cout << q.rep() << endl;
-	cout << "Executing Query for: " << q << endl;
-	print(cout, qr);
+	string sought1, sought2, sought3;
+	cout << "Input 3 words:" << endl;
+	while(cin >> sought1 >> sought2 >> sought3)
+	{
+		Query q = Query(sought1) & Query(sought2) | Query(sought3);
+		QueryResult qr = q.eval(tq);
+		cout << q.rep() << endl;
+		cout << "Executing Query for: " << q << endl;
+		print(cout, qr);
+	}
 	return 0;
 }
