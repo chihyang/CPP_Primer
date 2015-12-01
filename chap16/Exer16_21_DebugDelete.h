94f75fb0a5bad16a26b79802218fd470e00f1a10
chihyang <chihyanghsin@gmail.com>
2015-12-01 16:25:33 +0800

    Exercise 16.21 to 16.24.

---
+++ chap16/Exer16_21_DebugDelete.h
@@ -0,0 +1,15 @@
+#ifndef EXER16_21_DEBUG_DELETE_H
+#define EXER16_21_DEBUG_DELETE_H
+#include <iostream>
+// function-object class that calls delete on a given pointer
+class DebugDelete {
+public:
+	DebugDelete(std::ostream &s = std::cerr) : os(s) {}
+	// as with any function template, the type of T is deduced by the compiler
+	template <typename T>
+	void operator()(T *p) const
+	{ os << "deleting shared_ptr" << std::endl; delete p; }
+private:
+	std::ostream &os;
+};
+#endif
