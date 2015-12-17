d1ecc81765955e1a05829e270ae4350c7a7fc4a3
chihyang <chihyanghsin@gmail.com>
2015-12-17 20:07:18 +0800

    Fix return type of member functions of Screen in Exer07_29_Screen.h.

--- chap07/Exer07_29_Screen.h
+++ chap07/Exer07_29_Screen.h
@@ -9,11 +9,10 @@ public:
 	Screen(pos ht, pos wd, char c) : height(ht), width(wd), contents(ht * wd, c) {}
 	char get() const { return contents[cursor]; }
 	inline char get(pos ht, pos wd) const;
-	Screen move(pos r, pos c);
+	Screen& move(pos r, pos c);
 	Screen set(pos row, pos col, char c);
 	Screen set(char c);
-	Screen display(std::ostream &os);
-	const Screen display(std::ostream &os) const;
+	const Screen& display(std::ostream &os) const;
 private:
 	void do_display(std::ostream &os) const; // declare as const to apply to all cases
 	pos cursor = 0;
@@ -25,7 +24,7 @@ char Screen::get(pos r, pos c) const
 	pos row = r * width;
 	return contents[row + c];
 }
-Screen Screen::move(pos r, pos c)
+Screen& Screen::move(pos r, pos c)
 {
 	pos row = r * width;
 	cursor = row + c;
@@ -41,12 +40,7 @@ inline Screen Screen::set(char c)
 	contents[cursor] = c;
 	return *this;
 }
-Screen Screen::display(std::ostream &os)
-{
-	do_display(os);
-	return *this;
-}
-const Screen Screen::display(std::ostream &os) const
+const Screen& Screen::display(std::ostream &os) const
 {
 	do_display(os);
 	return *this;
