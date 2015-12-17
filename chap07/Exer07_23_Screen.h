d137b565ca137bfc31a6f16d7cd09127306ffaf4
chihyang <chihyanghsin@gmail.com>
2015-12-17 15:30:51 +0800

    Fix bugs in Exer07_23_Screen.h.

--- chap07/Exer07_23_Screen.h
+++ chap07/Exer07_23_Screen.h
@@ -5,8 +5,8 @@ class Screen{
 public:
 	typedef std::string::size_type pos;
 	Screen() = default;
-	Screen(pos ht, pos wd, char c) : height(ht), width(wd), contents(c) {}
-	char get() const { return contents[cursor] }
+	Screen(pos ht, pos wd, char c) : height(ht), width(wd), contents(ht * wd, c) {}
+	char get() const { return contents[cursor]; }
 	inline char get(pos ht, pos wd) const;
 	Screen &move(pos r, pos c);
 private:
@@ -17,7 +17,7 @@ private:
 char Screen::get(pos r, pos c) const 
 {
 	pos row = r * width;
-	return contens[row + c];
+	return contents[row + c];
 }
 Screen& Screen::move(pos r, pos c)
 {
