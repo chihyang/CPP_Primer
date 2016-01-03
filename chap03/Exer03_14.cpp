a1a325ade031d90ebf17e4de526f2981dae8a90b
chihyang <chihyanghsin@gmail.com>
2016-01-03 23:42:35 +0800

    Add solutions for exercise 3.14 and 3.15 in chapter 3.

---
+++ chap03/Exer03_14.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using std::cin;
+using std::vector;
+int main()
+{
+    vector<int> ivec;
+    int i;
+    while(cin >> i) {
+        ivec.push_back(i);
+	}
+    return 0;
+}
---
+++ chap03/Exer03_15.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using std::cin;
+using std::string;
+using std::vector;
+int main()
+{
+    vector<string> svec;
+    string str;
+    while(cin >> str) {
+        svec.push_back(str);
+	}
+    return 0;
+}
